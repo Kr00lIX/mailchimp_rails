3cee087b68c51117ad445ca0c37dfc983f0658ba
Anatoliy Kovalchuk <kr00lix@gmail.com>
2012-12-18 20:41:10 +0800

    add campaign links method

--- lib/mailchimp/core/campaign.rb
+++ lib/mailchimp/core/campaign.rb
@@ -32,7 +32,7 @@ module Mailchimp
       logger.debug "[Mailchimp.find_each] with '#{campaign_id}' campaign id"
       start = -1
       loop do
-        fetched_data = fetch_members(campaign_id, start += 1, batch_size)
+        fetched_data = members(campaign_id, start += 1, batch_size)
         break if fetched_data["data"].empty?
 
         fetched_data["data"].each do |member|
@@ -41,13 +41,19 @@ module Mailchimp
 
         sleep(rand(0.5))
       end
-      fetched_data["total"]
     end
 
-    def self.fetch_members(campaign_id, start = 0, limit = 1000)
+    def self.members(campaign_id, start = 0, limit = 1000)
       run do
         logger.debug "[Mailchimp.fetch_members] start=#{start}, limit=#{limit}"
         hominid.campaignMembers(campaign_id,  "", start, limit)
+      end
+    end
+
+    # http://apidocs.mailchimp.com/api/1.3/campaignclickstats.func.php
+    def self.links(campaign_id)
+      run do
+        hominid.campaignClickStats(campaign_id)
       end
     end
 
