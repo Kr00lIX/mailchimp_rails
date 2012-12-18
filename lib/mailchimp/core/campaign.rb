e9b939ceb92db80e95fd66a1e9d63e878b46b4ba
Anatoliy Kovalchuk <kr00lix@gmail.com>
2012-12-18 18:20:47 +0800

    add logging

--- lib/mailchimp/core/campaign.rb
+++ lib/mailchimp/core/campaign.rb
@@ -2,10 +2,10 @@ module Mailchimp
   class Campaign < Base
 
     def self.fetch_each(filters = {}, batch_size = 300)
-      logger.debug "Mailchimp.find_each with #{batch_size} batch size"
-      start = 0
+      logger.debug "[Mailchimp.find_each] with #{batch_size} batch size"
+      start = -1
       loop do
-        fetched_data = fetch(filters, start, batch_size)
+        fetched_data = all(filters, start += 1, batch_size)
         break if fetched_data["data"].empty?
 
         fetched_data["data"].each do |campaign|
@@ -13,7 +13,6 @@ module Mailchimp
         end
 
         sleep(rand(0.5))
-        start += 1
       end
     end
 
@@ -22,8 +21,30 @@ module Mailchimp
     #  :sendtime_start => # YYYY-MM-DD HH:mm:ss
     #
     # @note http://apidocs.mailchimp.com/api/1.3/campaigns.func.php
-    def self.fetch(filters = {}, start = 0, limit = 25)
+    def self.all(filters = {}, start = 0, limit = 25)
+      logger.debug "[Mailchimp.all] start=#{start}, limit=#{limit}"
       hominid.campaigns(filters, start, limit)
+    end
+
+    def self.fetch_each_member(campaign_id, batch_size = 1000)
+      logger.debug "[Mailchimp.find_each] with '#{campaign_id}' campaign id"
+      start = -1
+      loop do
+        fetched_data = fetch_members(campaign_id, start += 1, batch_size)
+        break if fetched_data["data"].empty?
+
+        fetched_data["data"].each do |member|
+          yield(member)
+        end
+
+        sleep(rand(0.5))
+      end
+      fetched_data["total"]
+    end
+
+    def self.fetch_members(campaign_id, start = 0, limit = 1000)
+      logger.debug "[Mailchimp.fetch_members] start=#{start}, limit=#{limit}"
+      hominid.campaignMembers(campaign_id,  "", start, limit)
     end
 
   end
