01dbe814e904daa1144cea338c94696a951317b4
Anatoliy Kovalchuk <kr00lix@gmail.com>
2014-02-10 11:17:59 +0200

    fix xmlrpc check Content-Length in response

--- lib/mailchimp.rb
+++ lib/mailchimp.rb
@@ -5,50 +5,33 @@ require "action_pack"
 
 module Mailchimp
 
-  if defined?(Rails) && Rails::VERSION::MAJOR == 3
-    extend ActiveSupport::Autoload
+  extend ActiveSupport::Autoload
 
-    autoload_under 'core' do
-      autoload :Base
-      autoload :List
-      autoload :UserList
-      autoload :UserModel
-      autoload :User
-      autoload :Group
-      autoload :Campaign
-      autoload :Ecommerce
-      autoload :WebHook
-      autoload :Util
-    end
-
-    autoload_under "controllers" do
-      autoload :WebHooksController
-    end
-
-    autoload_under 'errors' do
-      autoload :Error
-      autoload :NotEmplemented
-    end
-
-    require 'rails'
-    require 'mailchimp/engine'
-
-  else
-
-    require "mailchimp/core/base"
-    require "mailchimp/core/list"
-    require "mailchimp/core/user_list"
-    require "mailchimp/core/user_model"
-    require "mailchimp/core/user"
-    require "mailchimp/core/group"
-    require "mailchimp/core/campaign"
-    require "mailchimp/core/ecommerce"
-    require "mailchimp/core/web_hook"
-    require "mailchimp/core/util"
-    require "mailchimp/controllers/web_hooks_controller"
-    require "mailchimp/errors/error"
-    require "mailchimp/errors/not_emplemented"
+  autoload_under 'core' do
+    autoload :Base
+    autoload :List
+    autoload :UserList
+    autoload :UserModel
+    autoload :User
+    autoload :Group
+    autoload :Campaign
+    autoload :Ecommerce
+    autoload :WebHook
+    autoload :Util
   end
+
+  autoload_under "controllers" do
+    autoload :WebHooksController
+  end
+
+  autoload_under 'errors' do
+    autoload :Error
+    autoload :NotEmplemented
+  end
+
+  require 'rails'
+  require 'mailchimp/engine'
+
 
   if defined?(::ActiveRecord)
     ::ActiveRecord::Base.send(:include, Mailchimp::UserModel)
@@ -63,3 +46,4 @@ module Mailchimp
 end
 
 Mailchimp::Base.init_mailchimp unless defined?(Rails) && Rails::VERSION::MAJOR == 3
+XMLRPC::Client.include XMLRPC::ClientFixContentLenght if RUBY_VERSION.split(".").first.eql?("2")
